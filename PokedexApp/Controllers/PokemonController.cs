60b4bdfc9511a3c2dbb5c203a56de68fd0074eca
Bruno Rodri <devbrunorodri@gmail.com>
2023-12-04 16:38:30 -0300

    Atualização

--- PokedexApp/Controllers/PokemonController.cs
+++ PokedexApp/Controllers/PokemonController.cs
@@ -1,22 +1,52 @@
 ﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using PokedexApp.Models;
 using RestSharp;
+using static PokedexApp.Models.PokemonModel;
 
 namespace PokedexApp.Controllers;
 
 public class PokemonController : Controller
 {
+    //public IActionResult GetAllPokemon()
+    //{
+    //    var client = new RestClient("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0");
 
-    public IActionResult GetAllPokemon()
+    //    var request = new RestRequest("pokemon", Method.Get);
+    //    request.AddQueryParameter("name", "clefairy");
+
+    //    var response = await client.GetAsync(request);
+
+    //    return View();
+
+    //}
+
+    public IActionResult Index() 
     {
-        var client = new RestClient("https://pokeapi.co/api/v2/pokemon/");
-
-        var request = new RestRequest("pokemon", Method.Get);
-        request.AddQueryParameter("name", "clefairy");
-
-        // var response = await client.GetAsync(request);
-
-        return View();
+        GetAllPokemon();
+        return View(); 
     }
+
+    public async Task<Pokemon> GetAllPokemon()
+    {
+        Uri url = new Uri("https://pokeapi.co/api/v2/pokemon/1");
+        HttpClient client = new HttpClient();
+
+        var response = await client.GetAsync(url);
+        if (response.IsSuccessStatusCode)
+        {
+
+            string result = response.Content.ReadAsStringAsync().Result;
+            Pokemon pokemonModel = JsonConvert.DeserializeObject<Pokemon>(result);
+            
+            return pokemonModel;
+        }
+
+        return null;
+    }
+
+
 }
--- PokedexApp/Models/PokemonModel.cs
+++ PokedexApp/Models/PokemonModel.cs
@@ -1,7 +1,42 @@
+using Newtonsoft.Json;
+
 namespace PokedexApp.Models;
 
 public class PokemonModel
 {
-    public string name { get; set; }
-    public string url { get; set; }
+    public class Pokemon
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        public string name { get; set; }
+        public List<Type> types { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+        public List<Ability> abilities { get; set; }
+    }
+
+    public class Ability
+    {
+        public Ability2 ability { get; set; }
+        public bool is_hidden { get; set; }
+        public int slot { get; set; }
+    }
+
+    public class Ability2
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Type
+    {
+        public int slot { get; set; }
+        public Type2 type { get; set; }
+    }
+
+    public class Type2
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
 }
